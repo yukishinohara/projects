8caa3bec0e1027972fc67cc30b2c48790b02d0f0
yukishinohara <phyrsssrec@gmail.com>
2015-12-21 18:59:28 +0000

    Modify Log Pseudo likelyhood code
    http://deeplearning.net/tutorial/rbm.html

--- misc/rbmtest01/Rbm.py
+++ misc/rbmtest01/Rbm.py
@@ -28,14 +28,17 @@ class Rbm:
     def p_hv(self, v):
         return self.sigmoid(np.dot(v, self.w.T) + self.c)
 
-    def cd_k(self, v0):
-        h1 = rnd.rand(1, self.n)
-        v1 = rnd.rand(1, self.m)
+    def cd_k(self, v0, k=1):
+        k = 1 if k < 1 else k
+        vb = v0.copy()
+        for step in range(k):
+            hs = rnd.rand(1, self.n)
+            vs = rnd.rand(1, self.m)
+            hs = self.sgn(hs, self.p_hv(vb))
+            vs = self.sgn(vs, self.p_vh(hs))
+            vb = vs.copy()
 
-        h1 = self.sgn(h1, self.p_hv(v0))
-        v1 = self.sgn(v1, self.p_vh(h1))
-
-        return [h1, v1]
+        return [hs, vs]
 
     def train(self):
         dsize = np.size(self.D[:, 1])
@@ -73,28 +76,29 @@ class Rbm:
         t2 = np.sum(np.log(h_0 + h_1), axis=1)
         return -t1-t2
 
-    def log_pl_for(self, i):
-        Di = self.D.copy()
+    def log_pl_for(self, i, D=None):
+        D = self.D if D is None else D
+        Di = D.copy()
         Di[:, i] = 1 - Di[:, i]
-        fe_D  = self.free_energy(self.D)
+        fe_D  = self.free_energy(D)
         fe_Di = self.free_energy(Di)
         costs = self.m * np.log(self.sigmoid(fe_Di - fe_D))
         return np.mean(costs)
 
-    def log_pl_all(self):
+    def log_pl_all(self, D=None):
         idx = np.arange(self.m)
-        v_log_pl = np.vectorize(self.log_pl_for)
-        return v_log_pl(idx)
+        v_log_pl = np.vectorize(self.log_pl_for, excluded=[1])
+        return v_log_pl(idx, D)
 
-    def log_pl(self):
+    def log_pl(self, D=None):
         i = rnd.randint(self.m)
-        return self.log_pl_for(i)
+        return self.log_pl_for(i, D)
 
-    def log_pl_precise(self):
-        return np.mean(self.log_pl_all())
+    def log_pl_precise(self, D=None):
+        return np.mean(self.log_pl_all(D))
 
 
-def main(learningrate=0.7,epoch=300):
+def main(learningrate=0.6, epoch=300, verbose=2):
     data = np.array([[1, 1, 1, 0, 0, 0],
                      [1, 0, 1, 0, 0, 0],
                      [1, 1, 1, 0, 0, 0],
@@ -112,13 +116,15 @@ def main(learningrate=0.7,epoch=300):
 
     for ep in range(epoch):
         rbm.train()
-        print ('ep={}, E={}'.format(ep, rbm.log_pl_all()))
+        if verbose >= 2:
+            print ('ep={}, E={}'.format(ep, rbm.log_pl_all()))
 
-    dsize = np.size(tsdt[:, 1])
-    for l in range(dsize):
-        [h1, v1] = rbm.cd_k(tsdt[l:l+1, :])
-        energy = rbm.log_pl()
-        print ('h={}, v={}, E={}'.format(h1, v1, energy))
+    if verbose >= 1:
+        dsize = np.size(tsdt[:, 1])
+        for l in range(dsize):
+            [h1, v1] = rbm.cd_k(tsdt[l:l+1, :])
+            energy = rbm.log_pl_precise(tsdt[l:l+1, :])
+            print ('h={}, v={}, F={}'.format(h1, v1, energy))
 
     print (rbm.sim(tsdt))
 
