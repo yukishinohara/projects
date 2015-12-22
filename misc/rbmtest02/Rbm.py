04baeb27d2923707fe5e1f51365ff8b1491ff2aa
yukishinohara <phyrsssrec@gmail.com>
2015-12-22 15:58:42 +0000

    New RBM playground code
    with the data
    http://ibug.doc.ic.ac.uk/courses

---
+++ misc/rbmtest02/Rbm.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python
+
+import numpy.random as rnd
+import numpy as np
+
+
+class Rbm:
+    def __init__(self, D=None, m=0, n=0, a=0.1):
+        self.D = D
+        self.m = m
+        self.n = n
+        self.b = np.zeros((1, m))  # j visible
+        self.c = np.zeros((1, n))  # i hidden
+        self.w = np.zeros((n, m))
+        self.a = a
+
+    def sigmoid(self, x):
+        vf = np.vectorize(lambda y: 1/(1+np.exp(-y)))
+        return vf(x)
+
+    def sgn(self, x, th):
+        vf = np.vectorize(lambda y, t: 1 if y < t else 0)
+        return vf(x, th)
+
+    def p_vh(self, h):
+        return self.sigmoid(np.dot(h, self.w) + self.b)
+
+    def p_hv(self, v):
+        return self.sigmoid(np.dot(v, self.w.T) + self.c)
+
+    def cd_k(self, v0, k=1):
+        k = 1 if k < 1 else k
+        vb = v0.copy()
+        for step in range(k):
+            hs = rnd.rand(1, self.n)
+            vs = rnd.rand(1, self.m)
+            hs = self.sgn(hs, self.p_hv(vb))
+            vs = self.sgn(vs, self.p_vh(hs))
+            vb = vs.copy()
+
+        return [hs, vs]
+
+    def train(self):
+        dsize = np.size(self.D[:, 1])
+        dw = np.zeros((self.n, self.m))
+        db = np.zeros((1, self.m))
+        dc = np.zeros((1, self.n))
+        for l in range(dsize):
+            v0 = self.D[l:l+1, :]
+            [_, vk] = self.cd_k(v0)  # k = 1
+            p_hv0 = self.p_hv(v0)
+            p_hvk = self.p_hv(vk)
+            dw += np.dot(p_hv0.T, v0) - np.dot(p_hvk.T, vk)
+            db += v0 - vk
+            dc += p_hv0 - p_hvk
+        alpha = self.a / dsize
+        self.w += alpha*dw
+        self.b += alpha*db
+        self.c += alpha*dc
+
+    def sim(self, v):
+        h1 = self.p_hv(v)
+        v1 = self.p_vh(h1)
+        return self.sgn(np.full(v1.shape, 0.5), v1)
+
+    def sim2(self, v):
+        h1 = self.p_hv(v)
+        v1 = self.p_vh(h1)
+        return v1
+
+    def energy(self, h, v):
+        t1 = np.dot(np.dot(h, self.w), v.T)
+        t2 = np.dot(self.b, v.T)
+        t3 = np.dot(self.c, h.T)
+        return -t1-t2-t3
+
+    def free_energy(self, v):
+        t1 = np.dot(self.b, v.T)
+        h_1 = np.exp(np.dot(v, self.w.T) + self.c)
+        h_0 = np.full(h_1.shape, 1.)
+        t2 = np.sum(np.log(h_0 + h_1), axis=1)
+        return -t1-t2
+
+    def log_pl_for(self, i, D=None):
+        D = self.D if D is None else D
+        Di = D.copy()
+        Di[:, i] = 1 - Di[:, i]
+        fe_D  = self.free_energy(D)
+        fe_Di = self.free_energy(Di)
+        costs = self.m * np.log(self.sigmoid(fe_Di - fe_D))
+        return np.mean(costs)
+
+    def log_pl_all(self, D=None):
+        idx = np.arange(self.m)
+        v_log_pl = np.vectorize(self.log_pl_for, excluded=[1])
+        return v_log_pl(idx, D)
+
+    def log_pl(self, D=None):
+        i = rnd.randint(self.m)
+        return self.log_pl_for(i, D)
+
+    def log_pl_precise(self, D=None):
+        return np.mean(self.log_pl_all(D))
+
+
+def main(learningrate=0.6, epoch=300, verbose=2):
+    data = np.array([[1, 1, 1, 0, 0, 0],
+                     [1, 0, 1, 0, 0, 0],
+                     [1, 1, 1, 0, 0, 0],
+                     [0, 0, 1, 1, 1, 0],
+                     [0, 0, 1, 1, 0, 0],
+                     [0, 0, 1, 1, 1, 0],
+                     [0, 0, 1, 1, 1, 0]])
+
+    tsdt = np.array([[0, 0, 1, 1, 1, 0],
+                     [0, 1, 1, 0, 0, 0],
+                     [0, 0, 0, 0, 0, 0],
+                     [0, 0, 1, 0, 0, 1]])
+
+    rbm = Rbm(data, m=6, n=2, a=learningrate)
+
+    for ep in range(epoch):
+        rbm.train()
+        if verbose >= 2:
+            print ('ep={}, E={}'.format(ep, rbm.log_pl_all()))
+
+    if verbose >= 1:
+        dsize = np.size(tsdt[:, 1])
+        for l in range(dsize):
+            [h1, v1] = rbm.cd_k(tsdt[l:l+1, :])
+            energy = rbm.log_pl_precise(tsdt[l:l+1, :])
+            print ('h={}, v={}, F={}'.format(h1, v1, energy))
+
+    print (rbm.sim(tsdt))
+
+
+if __name__ == "__main__":
+    main()
+
---
+++ misc/rbmtest02/trial01.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+import numpy as np
+import h5py as h5
+import Rbm
+
+
+def main(ita=0.7, epoch=60, verbose=2, hidden=4, test_prop=5):
+    # Read test data 01
+    fp = h5.File('testdata01.h5', 'r')
+    x = fp['/clean/x'].value
+    y = fp['/clean/y'].value
+    data_size = x[:, 0].size
+    attr_size = x[0, :].size
+    feat_size = y[0, :].size
+    fp.close()
+
+    # Organize data
+    tran_size = int((data_size * (100 - test_prop)) / 100)
+    test_size = data_size - tran_size
+    z = np.zeros((test_size, feat_size))
+    inptd = np.c_[y[0:tran_size, :], x[0:tran_size, :]]
+    testd = np.c_[z, x[tran_size:, :]]  # error if my calculation has mistake
+    targy = y[tran_size:, :]
+
+    # Generate RBM
+    rbm = Rbm.Rbm(inptd, m=attr_size+feat_size, n=hidden, a=ita)
+
+    # Training
+    for ep in range(epoch):
+        rbm.train()
+        if verbose >= 3:
+            print ('ep={}, E={}'.format(ep, rbm.log_pl_all()))
+        elif verbose >= 1:
+            print ('\rep={}'.format(ep), end="")
+
+    # Re-construct
+    predy = (rbm.sim2(testd))[:, 0:feat_size]
+
+    # Answer check phase
+    t = np.argmax(targy, axis=1)
+    p = np.argmax(predy, axis=1)
+    print('')
+    if verbose >= 2:
+        for l in range(test_size):
+            energy = rbm.log_pl_precise(testd[l:l+1, :])
+            print ('p={}, y={}, F={}'.format(p[l], t[l], energy))
+    if verbose >= 1:
+        print ('predict = {}'.format(p))
+        print ('target  = {}'.format(t))
+    cr = np.count_nonzero((t-p)) / test_size
+    print ('CR = {}%'.format(cr*100))
+
+    return predy
+
+
+if __name__ == "__main__":
+    main()
+
