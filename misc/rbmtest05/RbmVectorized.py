b8a4160704d25719d4d5e66427af5b07e2ee5e45
yukishinohara <phyrsssrec@gmail.com>
2015-12-25 00:17:57 +0000

    Improve the efficiency

--- misc/rbmtest05/RbmVectorized.py
+++ misc/rbmtest05/RbmVectorized.py
@@ -2,36 +2,30 @@
 
 from __future__ import print_function
 import Rbm as rl
-import pyopencl as cl
 import numpy as np
 
 
 class RbmVectorized(rl.Rbm):
     def __init__(self, D=None, m=0, n=0, lr=0.1, mt=0., wd=0., bs=0):
         rl.Rbm.__init__(self, D, m, n, lr, mt, wd, bs)
-
-    def cd_1_batch(self, v0, batch_size):
-        hs = np.random.rand(batch_size, self.n)
-        vs = np.random.rand(batch_size, self.m)
-        hs = self.activate(hs, self.p_hv(v0))
-        vs = self.activate(vs, self.p_vh(hs))
-
-        return [hs, vs]
+        self.v_sigmoid = np.vectorize(lambda y: 1/(1+np.exp(-y)))
+        self.v_activate = np.vectorize(lambda y, t: 1 if y < t else 0)
 
     def train_by_mini_batch(self, batch, batch_size):
-        dw = np.zeros((self.n, self.m))
-        db = np.zeros((1, self.m))
-        dc = np.zeros((1, self.n))
-        v0 = batch
-        [_, vk] = self.cd_1_batch(v0, batch_size)  # Fix k = 1
-        p_hv0 = self.p_hv(v0)
-        p_hvk = self.p_hv(vk)
-        dw = np.dot(p_hv0.T, v0) - np.dot(p_hvk.T, vk)
-        db = v0 - vk
-        dc = p_hv0 - p_hvk
-        alpha = self.lr / batch_size
+        wT = self.w.T
+        h0 = np.random.rand(batch_size, self.n)
+        v1 = np.random.rand(batch_size, self.m)
+        p_hv0 = self.v_sigmoid(np.dot(batch, wT) + self.c)
+        h0 = self.v_activate(h0, p_hv0)
+        p_vh0 = self.v_sigmoid(np.dot(h0, self.w) + self.b)
+        v1 = self.v_activate(v1, p_vh0)
+        p_hv1 = self.v_sigmoid(np.dot(v1, wT) + self.c)
+        dw = np.dot(p_hv0.T, batch) - np.dot(p_hv1.T, v1)
+        db = batch - v1
+        dc = p_hv0 - p_hv1
         db = np.sum(db, axis=0)
         dc = np.sum(dc, axis=0)
+        alpha = self.lr / float(batch_size)
         self.delta_w = alpha*dw + self.mt*self.delta_w - self.wd*self.w
         self.delta_b = alpha*db + self.mt*self.delta_b - self.wd*self.b
         self.delta_c = alpha*dc + self.mt*self.delta_c - self.wd*self.c
